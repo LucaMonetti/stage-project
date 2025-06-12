ce48afc19203b7a4769e6d6b0f1cb2252df7b80e
Luca Monetti <luca.monetti@planetel.eu>
2025-06-12 11:55:05 +0200

    [feat] Companies Controller

---
+++ pricelist-manager.Server/Controllers/CompaniesController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using pricelist_manager.Server.Exceptions;
+using pricelist_manager.Server.Interfaces;
+using pricelist_manager.Server.Models;
+using System.Globalization;
+
+namespace pricelist_manager.Server.Controllers
+{
+    [ApiController]
+    [Route("api/companies")]
+    public class CompaniesController: ControllerBase
+    {
+        private ICompanyRepository CompanyRepository;
+
+        public CompaniesController(ICompanyRepository CompanyRepository)
+        {
+            this.CompanyRepository = CompanyRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var res = await CompanyRepository.GetAllAsync();
+
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var res = await CompanyRepository.GetByIdAsync(id);
+                return Ok(res);
+            }
+            catch (NotFoundException<Company> e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Company dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var data = dto;
+
+            try
+            {
+                var res = await CompanyRepository.CreateAsync(data);
+                return Ok(res);
+            }
+            catch (AlreadyExistException<Company> e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(string id, [FromBody] Company dto)
+        {
+            if (id != dto.Id)
+            {
+                ModelState.AddModelError("", "The IDs doesn't match!");
+                return BadRequest(ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var res = await CompanyRepository.UpdateAsync(dto);
+                return Ok(res);
+            }
+            catch (NotFoundException<Company> e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var res = await CompanyRepository.DeleteAsync(id);
+                return Ok(res);
+            }
+            catch (NotFoundException<Company> e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
