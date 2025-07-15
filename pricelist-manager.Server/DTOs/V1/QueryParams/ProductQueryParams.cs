045b02044019bb81c43790735391e2c47fcba0bd
Luca Monetti <luca.monetti@planetel.eu>
2025-07-15 17:15:08 +0200

    [feat] API product filters

--- pricelist-manager.Server/DTOs/V1/QueryParams/ProductQueryParams.cs
+++ pricelist-manager.Server/DTOs/V1/QueryParams/ProductQueryParams.cs
@@ -1,8 +1,16 @@
 ﻿namespace pricelist_manager.Server.DTOs.V1.QueryParams
 {
 
+    public class ProductFiltersQueryParams : QueryParameters
+    {
+        public string? ProductCode { get; set; }
+        public string? CompanyId { get; set; }
+    }
+
+
     public class ProductQueryParams : QueryParameters
     {
         public PaginationParameters Pagination { get; set; } = new PaginationParameters();
+        public ProductFiltersQueryParams? Filters { get; set; } = new ProductFiltersQueryParams();
     }
 }
