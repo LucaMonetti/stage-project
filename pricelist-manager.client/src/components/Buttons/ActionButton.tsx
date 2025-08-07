17d4004a8b62ce2284325ed30a1c99ec05f61058
Luca Monetti <luca.monetti@planetel.eu>
2025-08-07 15:39:33 +0200

    [fix] Small rendering issue with Action Buttons

--- pricelist-manager.client/src/components/Buttons/ActionButton.tsx
+++ pricelist-manager.client/src/components/Buttons/ActionButton.tsx
@@ -44,7 +44,7 @@ export default function ActionButton({
   return (
     <>
       <button
-        className={`text-white px-4 py-2 rounded hover:cursor-pointer ${backgroundColors[color]} flex items-center gap-2 whitespace-nowrap min-w-full ${className}`}
+        className={`text-white px-4 py-2 rounded hover:cursor-pointer ${backgroundColors[color]} flex items-center gap-2 whitespace-nowrap ${className}`}
         onClick={(e: React.MouseEvent) => {
           e.stopPropagation();
 
